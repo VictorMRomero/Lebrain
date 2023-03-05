bff8b308230a7e09c00c91c1c44cf67afe7330e9
Victor Manuel Romero Baez <vmrbaez@gmail.com>
2023-03-04 22:48:27 -0600

    Se modificaron los estilos de las tarjetas y se le dio funcionalidad a los botones

--- JS/app.js
+++ JS/app.js
@@ -1,72 +1,67 @@
-// Obtener las tarjetas y la barra de progreso
-var cards = document.querySelectorAll('.card');
-var progressBar = document.querySelector('.progress-bar');
-var questions = [  {    question: '¿En qué año comenzó la Segunda Guerra Mundial?',    answer: '1939'  },  {    question: '¿Quién escribió la novela "Cien años de soledad"?',    answer: 'Gabriel García Márquez'  },  {    question: '¿Cuál es la capital de Francia?',    answer: 'París'  }];
-var score = 0;
-
-// Definir el valor máximo de la barra de progreso y el valor actual
-var maxValue = 100;
-var currentValue = 0;
-
-// Función para actualizar el valor de la barra de progreso
-function updateProgressBar(value) {
-  progressBar.style.width = value + '%';
-  progressBar.setAttribute('aria-valuenow', value);
-}
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Aquí va todo el código JavaScript
+    // Obtener las tarjetas y la barra de progreso
+  var cards = document.querySelectorAll('.card');
+  var resultado = document.querySelector('.resultado');
+  var progressBar = document.querySelector('.progress-bar');
+  var cerrar = document.querySelector('.cerrar-btn')
 
+  // Definir el valor máximo de la barra de progreso y el valor actual
+  var maxValue = 100;
+  var currentValue = 0;
 
-// Función para mostrar la tarjeta actual y actualizar la barra de progreso
-function showCard(index, answer) {
-  // Ocultar todas las tarjetas
-  for (var i = 0; i < cards.length; i++) {
-    cards[i].classList.remove('active');
+  // Función para actualizar el valor de la barra de progreso
+  function updateProgressBar(value) {
+    progressBar.style.width = value + '%';
+    progressBar.setAttribute('aria-valuenow', value);
   }
-  // Mostrar la tarjeta actual
-  cards[index].classList.add('active');
-  // Verificar si la respuesta del usuario es correcta
-  if (answer === questions[index].answer) {
-    score++;
+
+  // Función para mostrar la tarjeta actual y actualizar la barra de progreso
+  function showCard(index) {
+    
+    // Ocultar todas las tarjetas
+    for (var i = 0; i < cards.length; i++) {
+      cards[i].classList.remove('active');
+    }
+  
+    if (currentValue === maxValue){
+      updateProgressBar(maxValue); 
+      cerrar.remove('active');
+      nextButton.textContent = 'Salir';
+      nextButton.id = 'finalizar';
+      resultado.classList.add('active');
+      var finalizar = document.querySelector('#finalizar');
+      finalizar.addEventListener('click', function(){
+        history.back();
+      });
+
+      // Mostrar la siguiente tarjeta sin la barra de progreso
+      
+    } else {
+      // Mostrar la tarjeta actual
+      cards[index].classList.add('active');   
+      // Actualizar el valor de la barra de progreso
+      currentValue += maxValue / cards.length;
+      updateProgressBar(currentValue);
+    }
   }
-  // Actualizar el valor de la barra de progreso
-  currentValue += maxValue / cards.length;
-  updateProgressBar(currentValue);
-}
 
+  // Mostrar la primera tarjeta y actualizar la barra de progreso
+  showCard(0);
 
-
-// Mostrar la primera tarjeta y actualizar la barra de progreso
-showCard(0);
-
-// Agregar un event listener al botón de "Siguiente"
-var nextButton = document.querySelector('#next-button');
-nextButton.addEventListener('click', function() {
-  // Obtener el índice de la tarjeta actual
-  var currentIndex = Array.from(cards).findIndex(function(card) {
-    return card.classList.contains('active');
+  // Agregar un event listener al botón de "Siguiente"
+  var nextButton = document.querySelector('#next-button');
+  nextButton.addEventListener('click', function() {
+    // Obtener el índice de la tarjeta actual
+    var currentIndex = Array.from(cards).findIndex(function(card) {
+      return card.classList.contains('active');
+    });
+    // Calcular el índice de la siguiente tarjeta
+    var nextIndex = (currentIndex + 1) % cards.length;
+    // Mostrar la siguiente tarjeta y actualizar la barra de progreso
+    showCard(nextIndex);
   });
-  // Calcular el índice de la siguiente tarjeta
-  var nextIndex = (currentIndex + 1) % cards.length;
-  // Mostrar la siguiente tarjeta y actualizar la barra de progreso
-  showCard(nextIndex);
-});
 
-
-nextButton.addEventListener('click', function() {
-  // Obtener el índice de la tarjeta actual
-  var currentIndex = Array.from(cards).findIndex(function(card) {
-    return card.classList.contains('active');
-  });
-  // Obtener la respuesta del usuario
-  var answer = document.querySelector('#answer').value;
-  // Mostrar la siguiente tarjeta y actualizar la barra de progreso
-  showCard(currentIndex, answer);
-  // Limpiar el campo de respuesta
-  document.querySelector('#answer').value = '';
-  // Verificar si se han contestado todas las preguntas
-  if (currentIndex === cards.length - 1) {
-    // Mostrar la calificación final
-    alert('Tu calificación es: ' + score + '/' + questions.length);
-  }
 });
